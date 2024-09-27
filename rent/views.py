4f508e2f3dfdab828b025e381a059c9014e59b86
Benjamin <benjaminoheneasare65@gmail.com>
2024-09-27 16:40:41 +0000

    changes

--- rent/views.py
+++ rent/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'rent/index.html')
+def property(request):
+    return render(request, 'rent/property.html')
+def propertyDetail(request):
+    return render(request, 'rent/propertyDetail.html')
