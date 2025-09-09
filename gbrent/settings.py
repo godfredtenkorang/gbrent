2b72327a55c2762158ad76e12f6604c4b89e6ee6
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2025-09-09 09:32:29 +0000

    changes

--- gbrent/settings.py
+++ gbrent/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['.eleobinvestmentltd.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.eleobinvestmentltd.com', '167.71.125.8', 'localhost', '127.0.0.1']
 
 
 # Application definition
